8fcafe69fab484a76c7eb38ff6828bdc69a3a66f
Andrii Kyreikov <Andropolog@gmail.com>
2021-11-03 23:12:31 +0200

    done

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -1,16 +1,13 @@
-const contacts = require("../../model/contacts.json");
+const { NotFound } = require("http-errors");
+const { contacts: service } = require("../../services");
 
-const getContactById = (req, res) => {
-  console.log(req.params)
+const getContactById = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = contacts.find((contact) => contact.id.toString() === contactId);
+  const result = await service.getById(contactId);
   if (!result) {
-    return res.status(404).json({
-      status: "error",
-      code: 404,
-      message: "Not found",
-    });
+    throw new NotFound(`Contact with id=${id} not found`);
   }
+
   res.json({
     status: "success",
     code: 200,
